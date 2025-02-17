e5e15d8d17629f5765a75f2bb9cc278161d3c8c6
Pranjal Singh <prancoder@gmail.com>
2025-02-17 14:14:33 +0530

    Testing Tailwind CSS Working

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <>
-        <div>Welcome to FinanceFlow</div>
-        <a href="/createAccount"><button className='bg-blue-500 p-2 border-black border-1 rounded-md mb-2'>Create Account</button></a>
-        <br />
-        <a href="/login"><button className="bg-indigo-500 hover:bg-fuchsia-500 p-2 border-black border-1 rounded-md">Login</button></a>
-    </>
+    <div className="m-3">
+      <div className="mb-2">Welcome to FinanceFlow</div>
+      <a href="/createAccount"><button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+        Create Account
+      </button></a>
+      <a href="/login"><button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded ml-2">
+        Login
+      </button></a>
+    </div>
   )
 }
 
