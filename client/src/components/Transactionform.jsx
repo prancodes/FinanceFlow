49a7c0bdd6326b78c15cb52037b4cd9209ad754e
pushkarsingh09 <pushkarsingh0306@gmail.com>
2025-02-17 20:08:07 +0530

    Added recurring interval in transaction form

--- client/src/components/Transactionform.jsx
+++ client/src/components/Transactionform.jsx
@@ -8,6 +8,7 @@ const Transactionform = () => {
   const [date, setDate] = useState("2024-12-14");
   const [description, setDescription] = useState("");
   const [isRecurring, setIsRecurring] = useState(false);
+  const [recurringInterval, setRecurringInterval] = useState("Daily");
 
   return (
     <div className="max-w-2xl mx-auto mt-3 p-6 bg-white rounded-xl">
@@ -91,20 +92,37 @@ const Transactionform = () => {
         />
       </div>
 
-        <div className="mb-6 flex items-center justify-between border border-gray-300 rounded-lg bg-gray-100 p-4">
-            <div>
-                <label className="text-black">Recurring Transaction</label>
-                <p className="text-gray-400 text-sm">Setup a recurring schedule for this transaction</p>
-            </div>
-            <button
-            className={`relative inline-flex items-center cursor-pointer w-10 h-6 rounded-full transition-colors focus:outline-none ${isRecurring ? 'bg-blue-500' : 'bg-gray-300'}`}
-            onClick={() => setIsRecurring(!isRecurring)}
-            >
-            <span
-            className={`inline-block w-4 h-4 bg-white rounded-full shadow-md transform transition-transform duration-300 ${isRecurring ? 'translate-x-5' : 'translate-x-1'}`}
-            ></span>
-            </button>
-        </div>
+    <div className="mb-6 flex items-center justify-between border border-gray-300 rounded-lg bg-gray-100 p-4">
+      <div>
+        <label className="text-black">Recurring Transaction</label>
+        <p className="text-gray-400 text-sm">Setup a recurring schedule for this transaction</p>
+      </div>
+      <button
+        className={`relative inline-flex items-center cursor-pointer w-10 h-6 rounded-full transition-colors focus:outline-none ${isRecurring ? 'bg-blue-500' : 'bg-gray-300'}`}
+        onClick={() => setIsRecurring(!isRecurring)}
+      >
+      <span
+        className={`inline-block w-4 h-4 bg-white rounded-full shadow-md transform transition-transform duration-300 ${isRecurring ? 'translate-x-5' : 'translate-x-1'}`}
+      ></span>
+      </button>
+    </div>
+
+    {isRecurring && (
+      <div className="mb-4">
+        <label className="block text-black mb-3">Recurring Interval</label>
+        <select
+          className="w-full p-2 h-10 border border-gray-300 rounded-lg bg-gray-100 focus:ring-gray-200"
+          value={recurringInterval}
+          onChange={(e) => setRecurringInterval(e.target.value)}
+        >
+          <option>Daily</option>
+          <option>Weekly</option>
+          <option>Monthly</option>
+          <option>Yearly</option>
+        </select>
+      </div>
+    )}
+
 
       <div className="flex space-x-4">
         <button className="w-1/2 px-4 py-2 hover:bg-gray-300 rounded-lg bg-gray-200">Cancel</button>
