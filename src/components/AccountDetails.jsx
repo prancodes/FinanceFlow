39b00141acceb8a3224102d74d4bee5c7287841c
Om Singh <omsingh06102005@gmail.com>
2025-02-22 13:40:54 +0530

    Added Analytics section to show the user expenses and income via graphs

--- src/components/AccountDetails.jsx
+++ src/components/AccountDetails.jsx
@@ -30,13 +30,15 @@ const AccountDetail = () => {
         <h1 className="text-3xl font-bold text-center text-blue-600 mb-6">
           {account.name} Account Details
         </h1>
+        <div className='flex items-center justify-center'>
         <button 
             type="button" 
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-5"
             onClick={handleViewAnalytics}
           >
             View Analytics
           </button>
+          </div>
         <p className="text-lg text-gray-700 mb-4">
           <strong>Type:</strong> {account.type}
         </p>
