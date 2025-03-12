20795eccdae803a6fa8297d8a1199a36d222486e
Pranjal Singh <prancoder@gmail.com>
2025-03-12 18:09:57 +0530

    SEO Optimization

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
+import sitemap from 'vite-plugin-sitemap';
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
+    sitemap({
+      hostname: 'https://financeflow.up.railway.app',
+    })
   ],
 });
