b8e0a9d81a969429794ffe9f93320ae2e61e5f07
Pranjal Singh <prancoder@gmail.com>
2025-04-05 23:53:23 +0530

    Secure connection for OTP delivery

--- middleware/Email.config.js
+++ middleware/Email.config.js
@@ -6,8 +6,8 @@ if (process.env.NODE_ENV !== "production") {
 
 export const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 587,
-    secure: false, // true for port 465, false for other ports
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
