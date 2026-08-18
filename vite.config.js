1444e45083e3222fc3be5a73536bad685f54ee88
Pranjal Singh <prancoder@gmail.com>
2026-08-18 19:53:27 +0530

    security: remove malicious injected code from vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import sitemap from 'vite-plugin-sitemap';
-import { createRequire } from 'module';
-
-const require = createRequire(import.meta.url);
 
 export default defineConfig(({ ssrBuild }) => ({
   plugins: [
